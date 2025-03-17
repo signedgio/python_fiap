3a4ec2cdbf3926ec5fd5625c093d0fb6e45909c2
signedgio <gioromanlopes2007@gmail.com>
2025-03-17 20:51:36 -0300

    meu primeiro programa em python. aula 1 da michele, dia 17/03/2025.

--- aula 1.py
+++ aula 1.py
@@ -1,1 +1,13 @@
-print("Hello World!")
+#print("Hello World!")
+"""print("Hello World!")"""
+
+check1 = 9
+check2 = 7.5
+check3 = 8
+
+media = (check1 + check2 + check3) /3
+print(media)
+#print((check1 + check2 + check3) /3)
+print(f"{media: .2f}")
+print(f"Média = {media: .2f}")
+#print(f"Média = {media}")
