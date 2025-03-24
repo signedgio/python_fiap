e32e2bcf32488576f5ab8300f477173ab9e91673
singedgio <gioromanlopes2007@gmail.com>
2025-03-24 19:54:50 -0300

    aula 2 da michele.

---
+++ aula 2/ex2.py
@@ -0,0 +1,5 @@
+
+a = 3
+b = 5
+formula = ((2*a) * (3*b))
+print(formula)
---
+++ aula 2/ex3.py
@@ -0,0 +1,11 @@
+a = 5
+b = 4
+c = 2
+conta = (a + b + c)
+print(conta)
+
+num1 = 4
+num2 = 5.5
+num3 = 6.2
+soma = (num1 + num2 + num3)
+print(soma)
