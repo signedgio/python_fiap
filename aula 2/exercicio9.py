463fa222f5edf436ae1d3c2e161557a2c8cd645d
signedgio <gioromanlopes2007@gmail.com>
2025-03-31 19:38:55 -0300

    exercício 9 corrigido.

--- aula 2/exercicio9.py
+++ aula 2/exercicio9.py
@@ -1,5 +1,5 @@
-distancia = float(input("qual é a distância a ser percorrida?: "))
-velocidade = float(input("qual a velocidade do veículo?: "))
+distancia = float(input("qual é a distância a ser percorrida em km?: "))
+velocidade_media= float(input("qual a velocidade do veículo em km/h?: "))
 
-tempo = distancia / velocidade
-print(f"este é o tempo do percurso: {tempo:.2f}" )
+tempo = distancia / velocidade_media
+print(f"este é o tempo do percurso: {tempo:.2f} horas" )
