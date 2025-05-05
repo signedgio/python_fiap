ce1aef26254c0e31f696c3b33c8c0a4a645ca2b4
signedgio <gioromanlopes2007@gmail.com>
2025-05-05 20:09:41 -0300

    coloca início 0 e final 10. não tava dando certo, mas deu quando coloquei esses números. eu sou meio burrinha.

--- aula 4/exercicio7.py
+++ aula 4/exercicio7.py
@@ -1,6 +1,6 @@
+t = int(input("Tabuada de: "))
 x = int(input("Início da tabuada: "))
 y = int(input("Final da tabuada: "))
-t = int(input("Tabuada de: "))
 while x <= y:
     print(x*t)
     x = x + 1
